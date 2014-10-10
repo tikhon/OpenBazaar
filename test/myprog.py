2e138dd98998722a89ccdfbee60a6b597c4fe841
Nikolaos Korasidis <renelvon@gmail.com>
2014-10-10 21:41:09 +0300

    Remove pointless test files.

--- test/myprog.py
+++
@@ -1,8 +0,0 @@
-import logging
-import sys
-
-def A(s):
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    logging.getLogger("MyProg.test_on_listing_results").setLevel(logging.DEBUG)
-    logging.getLogger("MyProg.test_on_listing_results").debug('Listings %s', s)
-    logging.getLogger("MyProg.test_on_listing_results").debug('Listings %s' % s)
